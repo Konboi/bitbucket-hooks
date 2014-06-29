226969f26b87fd43af9be9fc245ddbfed8d2ccf1
Konboi <ryosuke.yabuki@gmail.com>
2014-06-29 16:33:31 +0900

    Add logger metods and delete json parse

--- lib/bitbucket/hooks/server.rb
+++ lib/bitbucket/hooks/server.rb
@@ -15,15 +15,21 @@ module Bitbucket
         begin
           payload = req.params["payload"]
         rescue => e
+          info "#{e} - #{e.backtrace}"
           [400, [], ['BAD REQUEST']]
         end
-
         if req.request_method == 'POST' and payload
-          @event.on('push', JSON.parse(payload))
+          @event.on('push', payload)
           [200, [], ['OK']]
         else
           [400, [], ['BAD REQUEST']]
         end
+      end
+
+      private
+
+      def info(msg)
+        puts "#{Time.now.strftime('%H:%M:%S')} #{msg}"
       end
     end
   end
